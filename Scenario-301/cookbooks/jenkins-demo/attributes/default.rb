1635022343f4bc1583d04d41449e05ec1ea72033
Amrit Gill <amgill1234@gmail.com>
2017-11-15 19:54:58 -0500

    Removed trailing spaces

--- Scenario-301/cookbooks/jenkins-demo/attributes/default.rb
+++ Scenario-301/cookbooks/jenkins-demo/attributes/default.rb
@@ -12,10 +12,9 @@ default['jenkins_mdm']['script_dir'] = \
   '/var/lib/jenkins/code/scripts/devops-knowledgebase'
 default['jenkins_mdm']['jenkins_jobs'] = \
   'BuildMDMRepo,UpdateSandboxMDM,UpdateJenkinsItself,TailLogfile'
-default['jenkins_plugins'] = 
- {
-   'bouncycastle-api' => '2.16.0',
- 	'junit' => '1.18',
- 	'script-security' => '1.22',
- 	'matrix-project' => '1.7.1'  
- }
+default['jenkins_plugins'] = {
+'bouncycastle-api' => '2.16.0',
+'junit' => '1.18',
+'script-security' => '1.22',
+'matrix-project' => '1.7.1'
+}
