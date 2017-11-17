30915896f042ac9c961a1d882d719c72b04cc51c
dennyzhang <denny@dennyzhang.com>
2017-11-17 09:46:35 -0600

    add serverspec

--- Scenario-201/cookbooks/example/recipes/default.rb
+++ Scenario-201/cookbooks/example/recipes/default.rb
@@ -1,4 +1,11 @@
 # Run apt-get update first
 include_recipe 'apt::default'
 
-package 'jq'
+describe package('jq') do
+  it { should be_installed }
+end
+
+template '/etc/ec2-user/version.txt' do
+  source 'version.txt.erb'
+  mode 0o700  
+end
--- Scenario-201/cookbooks/example/test/integration/default/serverspec/server_spec.rb
+++ Scenario-201/cookbooks/example/test/integration/default/serverspec/server_spec.rb
@@ -6,3 +6,13 @@ require 'json'
 
 # Required by serverspec
 set :backend, :exec
+
+chef_data = JSON.parse(IO.read('/tmp/kitchen/dna.json'))
+
+update_version = \
+  chef_data.fetch('example').fetch('update_version')
+
+
+describe command('cat /etc/ec2-user/version.txt') do
+  its(:stdout) { should contain update_version }
+end
