cdd20a8fb320e7d267ac4629059572d638d733a5
dennyzhang <denny@dennyzhang.com>
2017-11-18 09:59:57 -0600

    update code

--- Scenario-301/cookbooks/jenkins-demo/attributes/default.rb
+++ Scenario-301/cookbooks/jenkins-demo/attributes/default.rb
@@ -5,11 +5,4 @@ default['jenkins_plugins'] = {
   'slack' => '2.3'
 }
 
-default['jenkins_mdm']['buildrepo_dir'] = '/var/lib/jenkins/code'
-default['jenkins_mdm']['avoid_external_network'] = '1'
-default['jenkins_mdm']['data_rentention_days'] = '7'
-
-default['jenkins_mdm']['docker_daemon_ip'] = '172.17.0.1'
-default['jenkins_mdm']['install_devkit'] = '0'
-
 default['jenkins_mdm']['jenkins_jobs'] = 'TailLogfile'
