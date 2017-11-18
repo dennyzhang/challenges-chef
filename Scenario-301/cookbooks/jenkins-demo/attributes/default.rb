4556583a0140a4221a520305283656e3c419f5af
dennyzhang <denny@dennyzhang.com>
2017-11-18 11:04:15 -0600

    update plugin

--- Scenario-301/cookbooks/jenkins-demo/attributes/default.rb
+++ Scenario-301/cookbooks/jenkins-demo/attributes/default.rb
@@ -2,11 +2,13 @@
 
 default['jenkins_plugins'] = {
   'thinBackup' => '1.9',
-  'command-launcher' => '1.0',
+  # TODO
+  # 'command-launcher' => '1.0',
   'bouncycastle-api' => '2.16.2',
   'credentials' => '2.1.16',
   'plain-credentials' => '1.4',
-  'slack' => '2.3'
+  'slack' => '2.3',
+  'script-security' => '1.35'
 }
 
 default['jenkins_demo']['jenkins_jobs'] = 'TailLogfile'
